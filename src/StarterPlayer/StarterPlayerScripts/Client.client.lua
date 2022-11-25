c4e8aecd78a761fd78d7df842dc469659659645f
evunei <itsnickyylol@gmail.com>
2022-11-25 10:41:34 -0500

    Client

--- src/StarterPlayer/StarterPlayerScripts/Client.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/Client.client.lua
@@ -1,1 +1,5 @@
-print("Hello world, from client!")
+for _,system in pairs(game.ReplicatedStorage.Modules.Client:GetChildren())do
+    if system:IsA("ModuleScript") then
+        local sys = require(system).new()
+    end
+end
