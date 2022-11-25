27cdef6cfb283ef3883f317fe5fe5983e0616349
evunei <itsnickyylol@gmail.com>
2022-11-25 11:14:48 -0500

    changed

--- src/ReplicatedStorage/Modules/Client/Dependencies/GUI/GUI.lua
+++ src/ReplicatedStorage/Modules/Client/Dependencies/GUI/GUI.lua
@@ -11,23 +11,46 @@ function module.new()
 
     };
     m1.timeOut = 0;
+    m1.guiObject = nil;
+    m1.isOpen = false;
 
     return m1
 end
 
 function module:Init()
+    if not (self.guiObject:IsA("Frame")) then assert(false,"GuiObject must be a Frame.") return end
     self:Open()
     task.delay(self.timeOut,function()
-        self:Close
+        self:Close()
     end)
 end
 
 function module:Open()
-    
+    if (self.isOpen) then return end
+    self.guiObject:TweenPosition(
+        UDim2.new(0.5,0,0.5,0), -- Final position the tween should reach
+        Enum.EasingDirection.In, -- Direction of the easing
+        Enum.EasingStyle.Sine, -- Kind of easing to apply
+        2, -- Duration of the tween in seconds
+        true, -- Whether in-progress tweens are interrupted
+        function ()
+            self.isOpen = true;
+        end -- Function to be callled when on completion/cancelation
+    )
 end
 
 function module:Close()
-    
+    if not(self.isOpen) then return end
+    self.guiObject:TweenPosition(
+        UDim2.new(0.5,0,-1,0), -- Final position the tween should reach
+        Enum.EasingDirection.In, -- Direction of the easing
+        Enum.EasingStyle.Sine, -- Kind of easing to apply
+        2, -- Duration of the tween in seconds
+        true, -- Whether in-progress tweens are interrupted
+        function ()
+            self.isOpen = false;
+        end -- Function to be callled when on completion/cancelation
+    )
 end
 
 return module
